39db32f635c311e1be07f79aa3564852777bc04b
Lara <laraftoller@gmail.com>
2021-12-11 10:56:35 -0300

    Criado controller de usuario, iniciando rotas autenticação

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import config from './config';
+import { apiRouter } from './routes/api.routes';
 
 
 const app = express();
-
 app.use(express.json());
+app.use(apiRouter);
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Conseguimos criar um servidor Express'
-    });
-});
 
 app.listen(config.PORT, async () => {
     console.log('Server funcionando na porta: ', config.PORT);
