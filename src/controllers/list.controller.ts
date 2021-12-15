ece438df34a4b737eabb7378c5c5ca1f6144e432
Lara <laraftoller@gmail.com>
2021-12-15 18:56:32 -0300

    INiciando controller e definindo rotas de Lista

---
+++ src/controllers/list.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { List } from '../models/list.model';
+import { Movie } from '../models/movie.model';
+import { paginate } from '../middlewares/pagination';
+
+function index() {
+
+}
+
+function add() {
+
+}
+
+function remove() {
+
+}
+
+export { index, add, remove }
